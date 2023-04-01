c0215decf1f47eb047ffc6b8dc7a26367a26451a
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2023-04-01 16:44:48 +0530

    Add files via upload

---
+++ src/dto/UserDTO.ts
@@ -0,0 +1,10 @@
+export interface UserDTO {
+    firstName: String, // String is shorthand for {type: String},
+    middleName: String,
+    lastName: String,
+    username: String,
+    password: String,
+    id: Number,
+    employeeId: Number,
+    createdDate: any
+}
